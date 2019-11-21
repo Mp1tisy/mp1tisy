0e696ceff6dd65f16a387a53e7f20415ec843368
yanpeiting <449108802@qq.com>
2019-11-21 23:48:36 +0800

    文章类别新增、删除、编辑功能完成

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -3,5 +3,10 @@ var commenUrl = 'http://localhost:8000/';
 var URLS = {
     user_login: commenUrl + 'admin/login',
     user_logout: commenUrl + 'admin/logout',
-    user_getuser: commenUrl + 'admin/getuser'
+    user_getuser: commenUrl + 'admin/getuser',
+
+    category_search:commenUrl+'admin/category_search',
+    category_delete:commenUrl+'admin/category_delete',
+    category_add:commenUrl+'admin/category_add',
+    category_edit:commenUrl+'admin/category_edit'
 }
